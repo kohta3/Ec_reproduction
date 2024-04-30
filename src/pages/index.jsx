e26d4d15584745bb7f9e16ab16b915141cd1f148
OkamuraKohta <gogonta4@gmail.com>
2024-04-30 17:35:08 +0900

    indexUpdate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import React from 'react'
 import HeaderSection from '../components/header/HeaderSection'
 import FooterSection from '../components/footer/FooterSection'
-import HomeMain from '../components/main/homeMain'
+import HomeMain from '../components/main/HomeMain'
 import RightSideBar from '../components/sidebar/RightSideBar'
 
 const inter = Inter({ subsets: ["latin"] });
